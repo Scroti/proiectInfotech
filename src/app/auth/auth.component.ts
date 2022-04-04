20c86b585485836d6d24b29a840c96a44e8d6bbe
p3\cmm <cristian.miahalache@p3-group.com>
2022-04-04 23:44:17 +0300

    created login page

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  isLoginMode: boolean = true;
+  constructor() {}
+  changeMode() {
+    this.isLoginMode = !this.isLoginMode;
   }
-
+  ngOnInit(): void {}
 }
